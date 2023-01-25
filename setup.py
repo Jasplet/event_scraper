95c07a51d20e49f47abcca6c81ccd935f471d63c
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2023-01-25 15:25:13 +0000

    added setup to try and install locally

--- setup.py
+++ setup.py
@@ -1,3 +1,2 @@
-from distutils.core import setup
-
-setup(name='Event_scraper', version='1.0', author='Joseph Asplet')
+from setuptools import setup, find_packages
+setup(name='Event_scraper', version='1.0', author='Joseph Asplet', packages=find_packages())
